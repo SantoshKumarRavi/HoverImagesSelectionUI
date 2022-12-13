5298704fbec95ab88e3ef93efed77277a32b8316
SantoshKumarRavi <drsanthosh1997@gmail.com>
2022-12-13 15:52:19 +0530

    first commit

---
+++ cardoverflow/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Home from "./layout/Home";
+function App() {
+  return (
+    <>
+     <Home/>
+    </>
+  )
+}
+export default App;
---
+++ cardoverflow/src/layout/Home.js
@@ -0,0 +1,101 @@
+import React from "react";
+// import { Button } from "react-bootstrap";
+// import {div} from "react-bootstrap";
+
+const Home = () => {
+  return (
+    <>
+      <div className="page-container">
+        <div className="header-container">
+          <div className="left-head-container">keepapp</div>
+          <div className="right-head-container">
+            <div>How it works</div>
+            <div>About us</div>
+            <div>Resources</div>
+            <div className="sign-up-btn">
+              <button>Sign Up</button>
+            </div>
+          </div>
+        </div>
+        <div className="body">
+          <div className="body-header"><h1>Your Mental Health Matter</h1></div>
+          <div className="body-message"><p>Take back control of your mental health.Train your brain with pleasure,develop memory,attention and thinkingwith</p></div>
+          <div className="body-btn">
+            <button>Try now 🡪</button>
+          </div>
+        </div>
+        <div className="bottom-container">
+          <div className="parent-wrapper">
+          <div class="parent">
+            <div class="childwrapper">
+              <div className="content-above">ABove content </div>
+              <div tabindex="-1" class="child">
+                <div className="card-top"> <p>Something</p></div>
+                <div className="card-bottom">
+                  <div><h1>Title</h1></div>
+                  <div><p>Some thing extra about the content</p></div>
+                </div>
+
+              </div>
+            </div>
+            <div class="childwrapper">
+            <div className="content-above">ABove content </div>
+              <div tabindex="-1" class="child">
+              <div className="card-top"> <p>Something</p></div>
+                <div className="card-bottom">
+                  <div><h1>Title</h1></div>
+                  <div><p>Some thing extra about the content</p></div>
+                </div>
+              </div>
+            </div>
+            <div class="childwrapper">
+            <div className="content-above">ABove content </div>
+              <div tabindex="-1" class="child">
+              <div className="card-top"> <p>Something</p></div>
+                <div className="card-bottom">
+                  <div><h1>Title</h1></div>
+                  <div><p>Some thing extra about the content</p></div>
+                </div>
+              </div>
+            </div>
+            <div class="childwrapper">
+            <div className="content-above">ABove content </div>
+              <div tabindex="-1" class="child">
+              <div className="card-top"> <p>Something</p></div>
+                <div className="card-bottom">
+                  <div><h1>Title</h1></div>
+                  <div><p>Some thing extra about the content</p></div>
+                </div>
+              </div>
+            </div>
+            <div class="childwrapper">
+            <div className="content-above">ABove content </div>
+              <div tabindex="-1" class="child">
+              <div className="card-top"> <p>Something</p></div>
+                <div className="card-bottom">
+                  <div><h1>Title</h1></div>
+                  <div><p>Some thing extra about the content</p></div>
+                </div>
+                </div>
+            </div>
+            <div class="childwrapper">
+            <div className="content-above">ABove content </div>
+              <div tabindex="-1" class="child">
+              <div className="card-top"> <p>Something</p></div>
+                <div className="card-bottom">
+                  <div><h1>Title</h1></div>
+                  <div><p>Some thing extra about the content</p></div>
+                </div>
+                
+                </div>
+            </div>
+        
+          </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
